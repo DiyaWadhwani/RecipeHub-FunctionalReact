28b814e211c4377827e5720cc9438096c11da1d4
Diya Wadhwani <wadhwani2399@gmail.com>
2024-03-25 04:26:16 -0700

    Modified more code to make it functional

--- recipe_hub/src/pages/RecipeDetailsPage.jsx
+++ recipe_hub/src/pages/RecipeDetailsPage.jsx
@@ -1,90 +1,98 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import { BiFork } from "react-icons/bi";
 import { IoArrowBackOutline } from "react-icons/io5";
 import { Link } from "react-router-dom";
 import EmptyHeader from "../fragments/EmptyHeader";
 import Footer from "../fragments/Footer";
 // import PropTypes from "prop-types";
-// import RecipeDetailsDisplay from "../displayContent/RecipeDetailsDisplay";
-import RecipeDetails from "../models/RecipeDetails";
-import Ingredient from "../models/Ingredient";
-import MyFirebaseDB from "../models/MyFirebaseDB";
+import RecipeDetailsDisplay from "../displayContent/RecipeDetailsDisplay";
+// import RecipeDetails from "../models/RecipeDetails";
+// import Ingredient from "../models/Ingredient";
+// import MyFirebaseDB from "../models/MyFirebaseDB";
 
-export default class RecipeDetailsPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      recipeDetails: {
-        recipeName: null,
-        recipeAuthor: null,
-        recipeInstructions: [],
-        recipeIngredients: [new Ingredient("Default Ingredient", "2")],
-      },
-    };
+export default function RecipeDetailsPage() {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     recipeDetails: {
+  //       recipeName: null,
+  //       recipeAuthor: null,
+  //       recipeInstructions: [],
+  //       recipeIngredients: [new Ingredient("Default Ingredient", "2")],
+  //     },
+  //   };
 
-    this.recipeDetails = new RecipeDetails();
-    this.myDatabase = new MyFirebaseDB();
-  }
+  //   this.recipeDetails = new RecipeDetails();
+  //   this.myDatabase = new MyFirebaseDB();
+  // }
 
-  componentDidMount() {
+  const [recipeDetails, setRecipeDetails] = useState({});
+  // const [recipeImageUrl, setRecipeImageUrl] = useState(null);
+
+  useEffect(() => {
     const urlSearchParams = new URLSearchParams(window.location.search);
     const recipeName = urlSearchParams.get("recipe_name");
     const isForked = urlSearchParams.get("isForked");
     console.log("isForked ?", isForked);
     if (recipeName) {
-      this.fetchRecipeDetailsFromBackend(recipeName, isForked);
+      fetchRecipeDetailsFromBackend(recipeName, isForked);
     }
-  }
+  }, []);
 
-  async fetchRecipeDetailsFromBackend(recipeName, isForked) {
+  // componentDidMount() {
+  //   const urlSearchParams = new URLSearchParams(window.location.search);
+  //   const recipeName = urlSearchParams.get("recipe_name");
+  //   const isForked = urlSearchParams.get("isForked");
+  //   console.log("isForked ?", isForked);
+  //   if (recipeName) {
+  //     this.fetchRecipeDetailsFromBackend(recipeName, isForked);
+  //   }
+  // }
+
+  const fetchRecipeDetailsFromBackend = async (recipeName, isForked) => {
     try {
       const fetchedRecipeDetails = await this.recipeDetails.fetchRecipeDetails(
         recipeName,
         isForked
       );
       if (fetchedRecipeDetails) {
-        this.setState({ recipeDetails: fetchedRecipeDetails });
+        setRecipeDetails(fetchedRecipeDetails);
       } else {
         console.log("Recipe not found");
       }
     } catch (error) {
       console.error("Error fetching recipe details:", error);
     }
-  }
+  };
 
-  render() {
-    const { recipeDetails, recipeImageUrl } = this.state;
-    console.log(recipeDetails);
-    console.log("imageURL -- ", recipeImageUrl);
-    return (
-      <>
-        <div className="body">
-          <EmptyHeader headerTag={recipeDetails.recipeName} />
-          {/* separation between Navbar and page content */}
-          <div className="sep-line"></div>
-          <div className="author-back-feature">
-            <Link to="/recipeList">
-              <IoArrowBackOutline className="back-arrow" />
-            </Link>
-            <div className="author-tag">
-              <p className="author-text">{recipeDetails.recipeAuthor}</p>
-            </div>
-          </div>
-          <Link
-            to={`/newUpdate?recipe_details=${encodeURIComponent(JSON.stringify(recipeDetails))}`}
-          >
-            <div className="fork-tag">
-              <BiFork className="fork-icon" />
-              <p className="fork-text">Fork</p>
-            </div>
+  return (
+    <>
+      <div className="body">
+        <EmptyHeader headerTag={recipeDetails.recipeName} />
+        {/* separation between Navbar and page content */}
+        <div className="sep-line"></div>
+        <div className="author-back-feature">
+          <Link to="/recipeList">
+            <IoArrowBackOutline className="back-arrow" />
           </Link>
-          {/* <RecipeDetailsDisplay recipeDetails={recipeDetails} /> */}
-          <div></div>
+          <div className="author-tag">
+            <p className="author-text">{recipeDetails.recipeAuthor}</p>
+          </div>
         </div>
-        <Footer />
-      </>
-    );
-  }
+        <Link
+          to={`/newUpdate?recipe_details=${encodeURIComponent(JSON.stringify(recipeDetails))}`}
+        >
+          <div className="fork-tag">
+            <BiFork className="fork-icon" />
+            <p className="fork-text">Fork</p>
+          </div>
+        </Link>
+        <RecipeDetailsDisplay recipeDetails={recipeDetails} />
+        <div></div>
+      </div>
+      <Footer />
+    </>
+  );
 }
 
 // RecipeDetails.propTypes = {
--- recipe_hub/src/pages/RecipeListPage.jsx
+++ recipe_hub/src/pages/RecipeListPage.jsx
@@ -1,118 +1,114 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import "../styling/RecipeList.css";
 import EmptyHeader from "../fragments/EmptyHeader";
 import Footer from "../fragments/Footer";
 import { Link } from "react-router-dom";
 import { IoArrowBackOutline } from "react-icons/io5";
-import RecipeDetails from "../models/RecipeDetails";
 
-export default class RecipeListPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      recipes: [],
-      isForked: false,
-    };
-    this.recipeDetails = new RecipeDetails();
-  }
+export default function RecipeListPage() {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     recipes: [],
+  //     isForked: false,
+  //   };
+  //   this.recipeDetails = new RecipeDetails();
+  // }
 
-  handleInputClick = () => {
+  const [recipes, setRecipes] = useState([]);
+  const [isForked, setIsForked] = useState(false);
+
+  function handleInputClick() {
     alert("This feature has been disabled!");
-  };
-
-  async componentDidMount() {
-    console.log("Component mounted");
-    await this.fetchRecipes();
-    // console.log("Recipes fetched:", this.state.recipes);
   }
 
-  async fetchRecipes() {
+  useEffect(() => {
+    console.log("Component mounted");
+    fetchRecipes();
+    // console.log("Recipes fetched:", this.state.recipes);
+  });
+
+  const fetchRecipes = async () => {
     try {
-      const { pathname } = new URL(window.location.href);
+      const pathname = new URL(window.location.href);
 
       if (pathname === "/myList") {
         // Fetching my created recipes
         console.log("Maintaining my recipes");
         const myRecipes = await this.recipeDetails.fetchMyRecipeNames();
         console.log("Recipes from fetchMyRecipeNames:", myRecipes);
-        this.setState({ recipes: myRecipes, isForked: false });
+        setRecipes(myRecipes);
+        setIsForked(false);
       } else if (pathname === "/myForkedList") {
         // Fetching my forked recipes
         console.log("Maintaining forked recipes");
         const forkedRecipes =
           await this.recipeDetails.fetchUserForkedRecipeNames();
         console.log("Recipes from fetchForkedRecipeNames:", forkedRecipes);
-        this.setState({ recipes: forkedRecipes, isForked: true });
+        setRecipes(forkedRecipes);
+        setIsForked(true);
       } else {
         // Fetching the entire list of recipes
         const allRecipes = await this.recipeDetails.fetchRecipeNames();
         console.log("Recipes from fetchRecipes:", allRecipes);
-        this.setState({ recipes: allRecipes, isForked: false });
+        setRecipes(allRecipes);
+        setIsForked(false);
       }
     } catch (error) {
       console.error("Error fetching recipes in RecipeList:", error);
     }
-  }
+  };
 
-  async containMyRecipes(recipes) {
-    this.recipeDetails.fetchMyCreatedRecipes(recipes);
-  }
+  return (
+    <>
+      <EmptyHeader headerTag="RecipeHub" />
+      <div className="body">
+        {/* separation between Navbar and page content */}
+        <div className="sep-line"></div>
 
-  render() {
-    const { recipes, isForked } = this.state;
-    console.log("Recipes in render:", recipes);
+        <Link to="/">
+          <IoArrowBackOutline className="back-arrow" />
+        </Link>
 
-    return (
-      <>
-        <EmptyHeader headerTag="RecipeHub" />
-        <div className="body">
-          {/* separation between Navbar and page content */}
-          <div className="sep-line"></div>
-
-          <Link to="/">
-            <IoArrowBackOutline className="back-arrow" />
+        <form className="find-recipe search-form d-flex">
+          <input
+            className="search-bar-input search-input form-control me-2"
+            type="search"
+            placeholder="Find a recipe"
+            aria-label="Search"
+            onClick={handleInputClick}
+            disabled
+          />
+          <div className="random-padding"></div>
+          <Link to="/newUpdate">
+            <button
+              className="new-button search-button btn btn-success"
+              type="submit"
+            >
+              New
+            </button>
           </Link>
+        </form>
 
-          <form className="find-recipe search-form d-flex">
-            <input
-              className="search-bar-input search-input form-control me-2"
-              type="search"
-              placeholder="Find a recipe"
-              aria-label="Search"
-              onClick={this.handleInputClick}
-              disabled
-            />
-            <div className="random-padding"></div>
-            <Link to="/newUpdate">
-              <button
-                className="new-button search-button btn btn-success"
-                type="submit"
-              >
-                New
-              </button>
-            </Link>
-          </form>
-
-          <div className="recipe-list">
-            {recipes && recipes.length > 0 ? (
-              <ul className="list-unstyled">
-                {recipes.map((recipe, index) => (
-                  <li key={index}>
-                    <Link
-                      to={`/recipe?recipe_name=${recipe}&isForked=${isForked}`}
-                    >
-                      {recipe}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            ) : (
-              <p>No recipes found.</p>
-            )}
-          </div>
-          <Footer />
+        <div className="recipe-list">
+          {recipes && recipes.length > 0 ? (
+            <ul className="list-unstyled">
+              {recipes.map((recipe, index) => (
+                <li key={index}>
+                  <Link
+                    to={`/recipe?recipe_name=${recipe}&isForked=${isForked}`}
+                  >
+                    {recipe}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>No recipes found.</p>
+          )}
         </div>
-      </>
-    );
-  }
+        <Footer />
+      </div>
+    </>
+  );
 }
