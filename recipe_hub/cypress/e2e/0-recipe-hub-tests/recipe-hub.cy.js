6086978afd310acb40a8fec7162555448a0b6cec
Diya Wadhwani <wadhwani2399@gmail.com>
2024-04-03 15:29:08 -0700

    uncommented first 2 tests

--- recipe_hub/cypress/e2e/0-recipe-hub-tests/recipe-hub.cy.js
+++ recipe_hub/cypress/e2e/0-recipe-hub-tests/recipe-hub.cy.js
@@ -1,52 +1,52 @@
 /* global cy */
 /// <reference types="cypress" />
 
-// describe("Login Form", () => {
-//   //Executes before each test
-//   beforeEach(() => {
-//     cy.visit("https://recipehub-functional.web.app/");
-//   });
+describe("Login Form", () => {
+  //Executes before each test
+  beforeEach(() => {
+    cy.visit("https://recipehub-functional.web.app/");
+  });
 
-//   //test1: Login form is displayed, user is logged in, homepage is displayed on correct credentials, error message is displayed on incorrect credentials
-//   it("Submits the login form with valid credentials", () => {
-//     cy.get("input[type='email']").type("diya@gmail.com");
-//     cy.get("input[type='password']").type("diya@2024");
-//     cy.get("button[type='submit']").click();
-//     cy.url().should("include", "/homepage");
-//     cy.contains("RecipeHub");
-//   });
+  //test1: Login form is displayed, user is logged in, homepage is displayed on correct credentials, error message is displayed on incorrect credentials
+  it("Submits the login form with valid credentials", () => {
+    cy.get("input[type='email']").type("diya@gmail.com");
+    cy.get("input[type='password']").type("diya@2024");
+    cy.get("button[type='submit']").click();
+    cy.url().should("include", "/homepage");
+    cy.contains("RecipeHub");
+  });
 
-//   it("Displays an error message with invalid credentials", () => {
-//     cy.get("input[type='email']").type("test@example.com");
-//     cy.get("input[type='password']").type("password");
-//     cy.get("button[type='submit']").click();
-//     cy.contains("Invalid email or password");
-//   });
-// });
+  it("Displays an error message with invalid credentials", () => {
+    cy.get("input[type='email']").type("test@example.com");
+    cy.get("input[type='password']").type("password");
+    cy.get("button[type='submit']").click();
+    cy.contains("Invalid email or password");
+  });
+});
 
-// describe("Navigation", () => {
-//   //test2: User is able to navigate through the pages
-//   it("Navigates through homepage", () => {
-//     cy.visit("https://recipehub-functional.web.app/underConstruction");
-//     cy.get("h1").contains("This page is under construction");
+describe("Navigation", () => {
+  //test2: User is able to navigate through the pages
+  it("Navigates through homepage", () => {
+    cy.visit("https://recipehub-functional.web.app/underConstruction");
+    cy.get("h1").contains("This page is under construction");
 
-//     cy.visit("https://recipehub-functional.web.app/recipelist");
-//     cy.get(".back-arrow").click();
-//     cy.url().should("include", "/homepage");
+    cy.visit("https://recipehub-functional.web.app/recipelist");
+    cy.get(".back-arrow").click();
+    cy.url().should("include", "/homepage");
 
-//     cy.visit("https://recipehub-functional.web.app/recipelist");
-//     cy.get(".recipe-list").should("contain", "No recipes found.");
-//     cy.get(".recipe-list").should("not.contain", "Loading...");
+    cy.visit("https://recipehub-functional.web.app/recipelist");
+    cy.get(".recipe-list").should("contain", "No recipes found.");
+    cy.get(".recipe-list").should("not.contain", "Loading...");
 
-//     cy.visit("https://recipehub-functional.web.app/myList");
-//     cy.get(".recipe-list").should("contain", "No recipes found.");
-//     cy.get(".recipe-list").should("not.contain", "Loading...");
+    cy.visit("https://recipehub-functional.web.app/myList");
+    cy.get(".recipe-list").should("contain", "No recipes found.");
+    cy.get(".recipe-list").should("not.contain", "Loading...");
 
-//     cy.visit("https://recipehub-functional.web.app/myForkedList");
-//     cy.get(".recipe-list").should("contain", "No recipes found.");
-//     cy.get(".recipe-list").should("not.contain", "Loading...");
-//   });
-// });
+    cy.visit("https://recipehub-functional.web.app/myForkedList");
+    cy.get(".recipe-list").should("contain", "No recipes found.");
+    cy.get(".recipe-list").should("not.contain", "Loading...");
+  });
+});
 
 describe("Create Recipe", () => {
   //test3: User is able to create a recipe
